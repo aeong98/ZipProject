3d6c709644dfa0a0934e7689ac11b7dcb3239bff
김서연 <sy9815@gmail.com>
2021-08-05 02:40:37 +0900

    frontend: add navbar component and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+  
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import Curations from './pages/Curations';
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+          <Route path='/curations' component={Curations} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Curations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Curations() {
+  return (
+    <div className='curations'>
+      <h1>Curations</h1>
+    </div>
+  );
+}
+
+export default Curations;
